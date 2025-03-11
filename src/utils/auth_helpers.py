c108d64382e28f8202573259f4f5668b74a9eeea
Serene <s.lobo@student.unsw.edu.au>
2025-03-11 19:01:59 +1100

    Update auth_helpers.py

---
+++ src/utils/auth_helpers.py
@@ -0,0 +1,24 @@
+from passlib.context import CryptContext
+import boto3
+from fastapi import HTTPException, status
+from src.db.dynamodb_client import user_table
+#hash-password helper function 
+
+
+# Initialize the password hashing context
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    """Hash the user's password using bcrypt"""
+    return pwd_context.hash(password)
+
+def save_user_to_dynamodb(user_item: dict):
+    """Save user data to DynamoDB"""
+    try:
+        user_table.put_item(Item=user_item)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error saving user to DynamoDB: {str(e)}"
+        )
+    
