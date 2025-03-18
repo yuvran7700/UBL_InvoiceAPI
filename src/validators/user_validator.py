c3e1f36b0810d3e010646a756aed61006a770e65
Serene <s.lobo@student.unsw.edu.au>
2025-03-18 15:41:37 +1100

    deleted old code

--- src/validators/user_validator.py
+++ src/validators/user_validator.py
@@ -1,6 +1,7 @@
+'''
+    Validates information required for successful user creation
+'''
 import abn
-# from fastapi import HTTPException, status
-# from src.db.dynamodb_client import user_table
 from src.exceptions.error_handler import ErrorContext, ValidationErrorHandler
 from src.exceptions.user_exceptions import (
     ABNValidationError,
@@ -9,16 +10,6 @@ from src.exceptions.user_exceptions import (
 )
 from src.repositories.user_repository import get_user
 
-#This file handles all the validation required for account creation
-
-#Validates the given ABN
-# def validate_abn(abn_value: str):
-#     """Validate Australian Business Number format."""
-#     if not abn.validate(abn_value):
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST,
-#             detail="Invalid ABN format"
-#         )
 
 def validate_abn(abn_value: str):
     """Validate Australian Business Number format."""
@@ -67,30 +58,3 @@ def check_email_exists(email: str):
         # Handle any unexpected errors and log them
         error_handler = ErrorContext(ValidationErrorHandler())
         error_handler.handle_error(Exception(f"Unexpected error while checking email: {str(e)}"))
-
-
-''''  
-def validate_password(password: str):
-    """Validate password strength."""
-    if len(password) < 8:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Password must be at least 8 characters long"
-        )
-    if not any(char.isdigit() for char in password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Password must contain at least one number"
-        )
-    if not any(char.isupper() for char in password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Password must contain at least one uppercase letter"
-        )
-    if not any(char.islower() for char in password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Password must contain at least one lowercase letter"
-        )
-
-'''
